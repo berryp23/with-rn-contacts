4096e9bc9b2c27cadd1b22376e49db852866f834
berry710 <smparkdev@gmail.com>
2024-03-04 00:01:38 +0900

    test3

--- build/src/android/withUpdateAppBuildGradle.d.ts
+++
@@ -1,4 +0,0 @@
-import { ConfigPlugin } from '@expo/config-plugins';
-declare const withUpdateAppBuildGradle: ConfigPlugin;
-export default withUpdateAppBuildGradle;
-//# sourceMappingURL=withUpdateAppBuildGradle.d.ts.map
